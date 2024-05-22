0fbb3ab0c96bf3e4a9f23d687150a78fcc67d4f7
thomasyttam <ytam5@myseneca.ca>
2024-05-22 12:03:27 -0400

    Excercise - Strings

---
+++ ex02_names.py
@@ -0,0 +1,5 @@
+firstName = input("What is your first name? ")
+middleName = input("What is your middle name? ")
+lastName = input("What is your last name? ")
+
+print("Your initials are: "+firstName[0]+middleName[0]+lastName[0])
---
+++ ex03_lotNumber.py
@@ -0,0 +1,4 @@
+lotNumber = "037-00901-00027"
+print("Country Code: " + lotNumber[:3])
+print("Product Code: " + lotNumber[4:9])
+print("Batch Number: " + lotNumber[10:])
