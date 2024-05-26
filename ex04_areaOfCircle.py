e482bf66f3db079fe01f8cee58c92e4f0ae616d0
thomasyttam <ytam5@myseneca.ca>
2024-05-26 11:24:55 -0400

    ex04 do

---
+++ ex04_areaOfCircle.py
@@ -0,0 +1,8 @@
+import math
+
+radius = float(input("Please input the radius of the circle: "))
+
+area = math.pi * (radius ** 2)
+circumference = 2 * math.pi * radius
+print("The area of the circle is: ", round(area,2))
+print("The cirumference of the circle is: ", round(circumference,2))
