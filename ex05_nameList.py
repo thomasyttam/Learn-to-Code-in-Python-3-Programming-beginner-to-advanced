551326afde9ba237fce36024247d0beebb757102
thomasyttam <ytam5@myseneca.ca>
2024-05-31 09:45:38 -0400

    ex05 done

---
+++ ex05_nameList.py
@@ -0,0 +1,5 @@
+nameList =["Alfie", "Beatrice", "Clare", "Thomas", "Vanessa"]
+print("The current name list is",nameList)
+newName = input("Please enter your name: ")
+nameList.append(newName)
+print("The new name list is",nameList)
