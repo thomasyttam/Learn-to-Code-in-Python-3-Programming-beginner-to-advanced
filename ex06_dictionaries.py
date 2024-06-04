3dfa9d004232a39376215545216101a351915b1c
thomasyttam <ytam5@myseneca.ca>
2024-06-04 17:31:27 -0400

    ex06 done

---
+++ ex06_dictionaries.py
@@ -0,0 +1,4 @@
+person = {"name":"Thomas", "gender":"male", "age":25, "address": "Norh York", "phone":"416-123-4567"}
+key = input("Please enter the information you want to know about the person: ").lower()
+inforequest = person.get(key,"Sorry, I don't know that information.")
+print(inforequest)
