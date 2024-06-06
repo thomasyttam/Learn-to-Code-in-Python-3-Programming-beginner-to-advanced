078ecd471b121a5053bacc204e7c01cbf770799f
thomasyttam <ytam5@myseneca.ca>
2024-06-05 23:12:27 -0400

    ex07 done

---
+++ ex07_bool.py
@@ -0,0 +1,8 @@
+myAge = 45
+yourAge = int(input("Enter your age: "))
+if yourAge > myAge:
+    print("You are older than me.")
+elif yourAge < myAge:
+    print("You are younger than me.")
+else:
+    print("We are the same age.")
