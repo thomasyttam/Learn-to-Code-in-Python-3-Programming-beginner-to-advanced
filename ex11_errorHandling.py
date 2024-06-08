0fd797904df600cf272554e67bc319c657fd0caf
thomasyttam <ytam5@myseneca.ca>
2024-06-08 19:03:42 -0400

    ex11done

--- ex11_errorHandling.py
+++ ex11_errorHandling.py
@@ -1,42 +1,51 @@
 data_valid = False
 
 while data_valid == False:
-    grade1 = float(input("Enter the grade of the first test: "))
-    if grade1 < 0 or grade1 > 10:
-        print("Grade should be between 0 and 10")
-        continue
-    else:
-        data_valid = True
+    try:
+        grade1 = float(input("Enter the grade of the first test: "))
+        if grade1 < 0 or grade1 > 10:
+            print("Grade should be between 0 and 10")
+        else:
+            data_valid = True
+    except:
+        print("Invalid input. Only numbers between 0 and 10 are accepted.")
 
 data_valid = False
 
 while data_valid == False:
-    grade2 = float(input("Enter the grade of the second test: "))
-    if grade2 < 0 or grade2 > 10:
-        print("Grade should be between 0 and 10")
-        continue
-    else:
-        data_valid = True
+    try:
+        grade2 = float(input("Enter the grade of the second test: "))
+        if grade2 < 0 or grade2 > 10:
+            print("Grade should be between 0 and 10")
+        else:
+            data_valid = True
+    except:
+        print("Invalid input. Only numbers between 0 and 10 are accepted.")
 
 data_valid = False
 
 while data_valid == False:
-    total_classes = float(input("Enter the total number of classes: "))
-    if total_classes <= 0:
-        print("The number of classes can't be zero or less")
-        continue
-    else:
-        data_valid = True
+    try:
+        total_classes = float(input("Enter the total number of classes: "))
+        if total_classes <= 0:
+            print("The number of classes can't be zero or less")
+        else:
+            data_valid = True
+    except: 
+        print("Invalid input. Only numbers greater than zero are accepted.")
+        
 
 data_valid = False
 
 while data_valid == False:
-    absences = float(input("Enter the number of absences: "))
-    if absences < 0 or absences > total_classes:
-        print("The number of absences can't be less than zero or greater than the total number of classes")
-        continue
-    else:
-        data_valid = True
+    try:
+        absences = float(input("Enter the number of absences: "))
+        if absences < 0 or absences > total_classes:
+            print("The number of absences can't be less than zero or greater than the total number of classes")
+        else:
+            data_valid = True
+    except:
+        print("Invalid input. Only numbers between 0 and", total_classes, "are accepted.")
 
 avg_grade = (grade1 + grade2) / 2
 attendance = (total_classes - absences) / total_classes
