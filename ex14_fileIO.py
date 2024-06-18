491c4d3e70b40b6ff1fa5a595df1a49c5c087e6b
thomasyttam <ytam5@myseneca.ca>
2024-06-18 14:19:40 -0400

    ex14 read excel file

--- ex14_fileIO.py
+++ ex14_fileIO.py
@@ -1,15 +1,30 @@
 # Desription: This program demonstrate the use of file I/O.
 
+import pandas as pd
+
 # create a file
-# try to create a file
 fileName = input("Please enter the name of the file you want to create: ")
+
 try:
     file = open(fileName, "x")
     file.close()
 except:
     print("The file already exists.")
 
+# read the file
 file = open(fileName, "r")
 
 fileContext=file.read()
 print(fileContext)
+
+file.close()
+
+# write to the file
+
+file = open(fileName, "w")
+fileInput = input("Please enter the text you want to write to the file: ")
+file.write(fileInput)
+file.close()
+
+excelContent = pd.ExcelFile("sales.xlsx")
+print(excelContent)
